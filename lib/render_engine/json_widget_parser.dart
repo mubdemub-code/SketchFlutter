748fc6f5b1f7cb90e3df8de1b96d6f76cd356d4c
mubdemub-code <mubdemub@gmail.com>
2026-08-18 17:35:40 +0200

    Update json_widget_parser.dart

--- lib/render_engine/json_widget_parser.dart
+++ lib/render_engine/json_widget_parser.dart
@@ -3,7 +3,25 @@ import 'package:flutter/material.dart';
 import '../models/project_model.dart';
 import '../models/widget_node.dart';
 import 'render_context.dart';
-import 'widget_builders/...'; // importer tous les builders
+import 'widget_builders/scaffold_builder.dart';
+import 'widget_builders/container_builder.dart';
+import 'widget_builders/text_builder.dart';
+import 'widget_builders/row_column_builder.dart';
+import 'widget_builders/button_builder.dart';
+import 'widget_builders/image_builder.dart';
+import 'widget_builders/icon_builder.dart';
+import 'widget_builders/text_field_builder.dart';
+import 'widget_builders/checkbox_builder.dart';
+import 'widget_builders/switch_builder.dart';
+import 'widget_builders/slider_builder.dart';
+import 'widget_builders/list_view_builder.dart';
+import 'widget_builders/grid_view_builder.dart';
+import 'widget_builders/list_tile_builder.dart';
+import 'widget_builders/app_bar_builder.dart';
+import 'widget_builders/sized_box_builder.dart';
+import 'widget_builders/padding_builder.dart';
+import 'widget_builders/center_builder.dart';
+  // importer tous les builders
 
 /// Parser principal JSON → Widget Flutter.
 ///
