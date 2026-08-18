4aac20c5dc76fcb74e95bcb3528358de2bcc916c
mubdemub-code <mubdemub@gmail.com>
2026-08-18 11:22:19 +0200

    Update editor_screen.dart

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/project_model.dart';
 
 import '../core/constants/app_colors.dart';
 import '../core/constants/app_strings.dart';
